2ea8552e6c2bf9c0b4b0f89aff9e5c7036451bfe
openusm <34766901+openusm@users.noreply.github.com>
2018-04-04 10:51:00 +0530

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -41,7 +41,7 @@ EOF
   docker info
 
   # Docker Compose
-  sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+  sudo curl -L https://github.com/docker/compose/releases/download/1.21.0-rc1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
   sudo chmod +x /usr/local/bin/docker-compose
   ## show docker-compose version
   docker-compose version
